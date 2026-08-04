882a6c7509ce66c2334991c70207794d29f7dc46
bahrus <anderson.bruce.b@gmail.com>
2026-08-04 17:02:02 -0400

    Initial commit

---
+++ FontFaceFeature.js
@@ -0,0 +1,33 @@
+//@ts-check
+/** @import { FontFaceFeatureConfig } from './types/font-face-feature/types.d.ts'; */
+
+class FontFaceFeature {
+
+    /**
+     * Called once by `assignFeatures` after registration.
+     * Pre-loads processor modules and installs prototype getter/setters.
+     * Also ensures sourceOfTruth attributes are added to observedAttributes.
+     * 
+     */
+
+
+    /**
+     * 
+     * @param {{ new(...args: any[]): any; prototype: any; observedAttributes?: string[] }} ctr 
+     * @param {{customData: FontFaceFeatureConfig; withAttrs?: Record<string, any>; [key: string]: any}} featureConfig 
+     */
+    static async onAssigned(
+        ctr,
+        featureConfig
+    ){
+        const { customData, withAttrs } = featureConfig;
+        const { fontFamily } = customData;
+        const fontFamilies = Array.isArray(fontFamily) ? fontFamily : [fontFamily];
+        for(const fontConfig of fontFamilies){
+            const { name, url, descriptors } = fontConfig;
+            const fontFace = new FontFace(name, `url(${url})`, descriptors);
+            await fontFace.load();
+            document.fonts.add(fontFace);
+        }
+    }
+}
